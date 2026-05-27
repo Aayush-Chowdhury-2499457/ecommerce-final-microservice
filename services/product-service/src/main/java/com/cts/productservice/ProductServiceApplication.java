94da2c7e029418ad750aa72414cf08f073cc3171
ArijitGhosh <Arijit.Ghosh2@cognizant.com>
2026-05-27 14:15:17 +0530

    branch arijit  created

--- services/product-service/src/main/java/com/cts/productservice/ProductServiceApplication.java
+++ services/product-service/src/main/java/com/cts/productservice/ProductServiceApplication.java
@@ -3,6 +3,7 @@ package com.cts.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProductServiceApplication {
 
